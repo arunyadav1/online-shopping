abc5923428f105bdcd65ad2a0b23dba1a1fff099
Arun Yadav <arunyadav190@gmail.com>
2017-10-17 12:24:29 +0530

    My project setup

---
+++ shoppingbackend/src/main/java/net/arn/shoppingbackend/App.java
@@ -0,0 +1,13 @@
+package net.arn.shoppingbackend;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
